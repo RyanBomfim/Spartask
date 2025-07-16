28f5f88fc14cc140ae0c59f99e9e9c3074cb8e9f
Ryan de Oliveira <ryanocbomfim@gmail.com>
2025-07-16 01:31:03 -0300

    Upgrade Front-end index.php

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['usuario'])) {
        METADADOS E TÍTULO
   -------------------------------------------------- -->
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Spartask - Login e Cadastro AJAX</title>
 
   <!-- --------------------------------------------------
@@ -25,6 +26,7 @@ if (isset($_SESSION['usuario'])) {
   <link rel="stylesheet" href="assets/css/session.css">
   <style>
     body {
+      background-image: url('assets/img/carrosel1.jpg');
       background-size: cover;
       margin: 0;
       height: 100vh;
@@ -45,36 +47,22 @@ if (isset($_SESSION['usuario'])) {
 
 <body>
 
+
   <!-- --------------------------------------------------
      CARTÃO DE AUTENTICAÇÃO
 -------------------------------------------------- -->
 
- <div class="col-md-6">
-    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+<div class="col-md-8">
+   <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm bg-white position-relative"
+   style="max-height: 500px !important;">
       <div class="col p-4 d-flex flex-column position-static"> <strong
-          class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-        <h3 class="mb-0">Featured post</h3>
-        <div class="mb-1 text-body-secondary">Nov 12</div>
-        <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional
-          content.</p> <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
-          Continue reading
-          <svg class="bi" aria-hidden="true">
-            <use xlink:href="#chevron-right"></use>
-          </svg> </a>
-      </div>
-      <div class="col-auto d-none d-lg-block"> <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img "
-          height="250" preserveAspectRatio="xMidYMid slice" role="img" width="200" xmlns="http://www.w3.org/2000/svg">
-          <title>Placeholder</title>
-          <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
-            dy=".3em">Thumbnail</text>
-        </svg> </div>
-    </div>
-  </div>
+          class="d-inline-block mb-2 text-primary-emphasis">Spartask</strong>
 
-  <div class="auth-card">
+
+  <div class="auth-card ">
     <!-- CADASTRO -->
     <div id="cadastroView">
-      <h2 class="text-center mb-4">Cadastre-se grátis</h2>
+      <h2 class=" mb-4">Cadastre-se grátis</h2>
       <form id="formCadastro">
         <div class="form-floating mb-3">
           <input type="email" name="email" class="form-control" required />
@@ -108,6 +96,17 @@ if (isset($_SESSION['usuario'])) {
             🧼</span></p>
       </form>
     </div>
+  </div>
+  </div>
+
+   <div class="col-auto d-none d-lg-block">
+     <img src="assets/img/banner.png"
+     aria-label="Placeholder: Thumbnail" class="bd-placeholder-img"
+     style="height: 100% !important;" preserveAspectRatio="xMidYMid slice" role="img" width="400"
+           >
+         </div>
+  </div>
+  </div>
   </div>
 
   <!-- --------------------------------------------------
