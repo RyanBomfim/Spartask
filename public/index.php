ae73660734243a3e8fb2020578ff6e65f9aaeb7b
Ryan Bomfim <83607028+Ryan1503@users.noreply.github.com>
2025-07-18 01:23:23 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // --------------------------------------------------
-// INÍCIO DA SESSÃO E REDIRECIONAMENTO SE JÁ LOGADOaaaaaaaaaaa
+// INÍCIO DA SESSÃO E REDIRECIONAMENTO SE JÁ LOGADOaaaaaaaaaaaa
 // --------------------------------------------------a
 session_start();
 if (isset($_SESSION['usuario'])) {
