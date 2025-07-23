c80aae45d3b610a5014d88519158413f10848680
Ryan de Oliveira <ryanocbomfim@gmail.com>
2025-07-22 23:10:01 -0300

    Upgrade in the project

--- public/config/config.php
+++ public/config/config.php
@@ -1,15 +1,11 @@
 <?php
-$host = 'dpg-d202m7bipnbc73ai6vt0-a.oregon-postgres.render.com';
-$port = '5432';
-$dbname = 'spartask';
-$usuario = 'spartask_user';
-$senha = 'WhXkz6P2fEkMpMJz5CSslqiCLYCnBQQZ';
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "spartask-pro";
 
-try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $usuario, $senha);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "✅ Conectado com sucesso ao PostgreSQL!";
-} catch (PDOException $e) {
-    die("❌ Erro na conexão: " . $e->getMessage());
+$conn = new mysqli($host, $usuario, $senha, $banco);
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
 }
 ?>
